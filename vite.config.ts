5b38a694b0882fc917a5d595e57be4a7570b1268
Hammed Oyedele <itz.harmid@gmail.com>
2024-10-22 22:07:36 +0100

    fix: pagination issues with infinite scrolling

--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,20 @@
 import path from 'path'
-import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { defineConfig, loadEnv } from 'vite'
 
-export default defineConfig({
-  plugins: [react()],
-  base: '/lovemeetstech2024',
-  resolve: {
-    alias: {
-      '@': path.resolve(__dirname, './src'),
-    }
-  },
-  server: {
-    open: true,
-  },
+export default defineConfig(({mode}) => {
+  const env = loadEnv(mode, process.cwd());
+
+  return {
+    plugins: [react()],
+    base: env.VITE_BASE_URL,
+    resolve: {
+      alias: {
+        '@': path.resolve(__dirname, './src'),
+      }
+    },
+    server: {
+      open: true,
+    },
+  };
 });
