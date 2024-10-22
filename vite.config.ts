16cef56e2988240a73f77dbc91f7b5b4bd52b304
Hammed Oyedele <itz.harmid@gmail.com>
2024-10-22 21:56:42 +0100

    fix: pagination issues with infinite scrolling

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/love-meets-tech-2024',
+  base: '/lovemeetstech2024',
   build: {
     outDir: 'docs',
   },
